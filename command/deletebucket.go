1cd6d6117024136182af0341786bda4f58c04730
Troin Artis <devon@devonartis.com>
2018-04-18 16:05:45 -0400

    Updated files

--- command/deletebucket.go
+++ command/deletebucket.go
@@ -2,6 +2,7 @@ package command
 
 import "github.com/codegangsta/cli"
 
+//CmdDeletebucket command to delete S3 bucket
 func CmdDeletebucket(c *cli.Context) {
 
 }
--- command/listbuckets.go
+++ command/listbuckets.go
@@ -2,6 +2,7 @@ package command
 
 import "github.com/codegangsta/cli"
 
+//CmdListbuckets list S3 buckets command
 func CmdListbuckets(c *cli.Context) {
 
 }
